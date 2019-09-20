c6bdee49cb3ac42cf713de6cedd7d2e6055c0990
Ingmar Jager <ingmarjager@gmail.com>
2019-09-20 13:37:24 +0200

    Semihosting example: print data to host in different ways
    - hprintln
    - dbg! macro (very slow, but useful)
    - write!() in combination with core::fmt

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -3,8 +3,6 @@
 
 // pick a panicking behavior
 extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// extern crate panic_abort; // requires nightly
-// extern crate panic_itm; // logs messages over ITM; requires ITM support
 // extern crate panic_semihosting; // logs messages to the host stderr; requires a debugger
 
 use cortex_m::asm;
@@ -20,27 +18,27 @@ extern crate stm32f1xx_hal;
 #[entry]
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-    hprintln!("Hello, rust world!").unwrap();
-    print();
 
-    const UUID: *mut u32 = 0x0009_FC70 as *mut u32;
-    dbg!(UUID);
-    let mut uuid: [u32; 4] = [0; 4];
-    for i in 0..4 {
-        dbg!(i);
-        uuid[i] = unsafe { dbg!(UUID.offset(i as isize).read_volatile()) };
-    }
+    semihosting_print_example().ok();
 
     loop {
         // your code goes here
     }
 }
 
-// This function will be called by the application
+fn semihosting_print_example() -> Result<(), core::fmt::Error> {
 
+    hprintln!("Hello, rust world!").unwrap();
 
-// This function will be called by the application
-fn print() -> Result<(), core::fmt::Error> {
+    const UUID: *mut u32 = 0x1FFF_F7E8 as *mut u32;
+    dbg!(UUID);
+
+    let mut uuid: [u32; 4] = [0; 4];
+    for i in 0..4 {
+        dbg!(i);
+        uuid[i] = unsafe { dbg!(UUID.offset(i as isize).read_volatile()) };
+    }
+
     let mut stdout = match hio::hstdout() {
         Ok(fd) => fd,
         Err(()) => return Err(core::fmt::Error),
