9310a579fb6754d315961776797452a911fcdf17
Ingmar Jager <ingmarjager@gmail.com>
2019-09-20 14:37:39 +0200

    Add pin interrupts that trigger on fallinig edge
    These two pins are connected to the A and B channel of the optical
    encoder.
    
    For now we only blink a led in the interrupt routine, however the next
    step is to keep the encoder count

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -15,18 +15,114 @@ use cortex_m_semihosting::{hprintln, hio};
 #[cfg(feature = "use_semihosting")]
 use core::fmt::Write;
 
-extern crate stm32f1xx_hal;
+// extern crate stm32f1xx_hal;
+use stm32f1xx_hal::{
+    prelude::*, // auto import of most used stuff
+    gpio::*, // gpio hal implementation for stm32f1xx
+    pac::{self, interrupt, EXTI} // peripheral access crate (register access)
+};
+use embedded_hal::digital::v2::OutputPin;
+use core::{cell::RefCell, ops::DerefMut};
+use cortex_m::{interrupt::Mutex};
+
+// Make external interrupt registers globally available
+static INT: Mutex<RefCell<Option<EXTI>>> = Mutex::new(RefCell::new(None));
+
+// Make our LED globally available
+static LED: Mutex<RefCell<Option<gpiob::PB12<Output<PushPull>>>>> = Mutex::new(RefCell::new(None));
+
 
 #[entry]
 fn main() -> ! {
-
     #[cfg(feature = "use_semihosting")]
     semihosting_print_example().ok();
+
+    // Get access to the core peripherals from the cortex-m crate
+    // let cp = cortex_m::Peripherals::take().unwrap();
+    // Get access to the device specific peripherals from the peripheral access crate
+    let dp = pac::Peripherals::take().unwrap();
+
+    let mut rcc = dp.RCC.constrain();
+    let mut gpiob = dp.GPIOB.split(&mut rcc.apb2);
+
+    let mut led = gpiob.pb12.into_push_pull_output(&mut gpiob.crh);
+
+    // turn on led (inverted logic)
+    led.set_low().unwrap();
+
+
+    // input pin and interrupt setup
+    // PA5 ChA, PA10 = ChB
+    let mut gpioa = dp.GPIOA.split(&mut rcc.apb2);
+    let _pin_a5 = gpioa.pa0.into_pull_up_input(&mut gpioa.crl);
+    let _pin_a10 = gpioa.pa7.into_pull_up_input(&mut gpioa.crl);
+
+
+    let exti = dp.EXTI;
+
+    // Set interrupt request masks
+    exti.imr.modify(|_, w| {
+        w.mr5().set_bit();
+        w.mr10().set_bit()
+    });
+
+    // Set interrupt falling triggers
+    exti.ftsr.modify(|_, w| {
+        w.tr5().set_bit();
+        w.tr10().set_bit()
+    });
+
+    // Enable the external interrupt in the NVIC.
+    unsafe {
+        pac::NVIC::unmask(pac::Interrupt::EXTI15_10);
+        pac::NVIC::unmask(pac::Interrupt::EXTI9_5);
+    }
+
+    // Move control over LED and DELAY and EXTI into global mutexes
+    cortex_m::interrupt::free(|cs| {
+        *LED.borrow(cs).borrow_mut() = Some(led);
+        *INT.borrow(cs).borrow_mut() = Some(exti);
+    });
+
 
     loop {
         // your code goes here
     }
 }
+
+#[interrupt]
+fn EXTI9_5() {
+    cortex_m::interrupt::free(|cs| {
+        if let Some(ref mut exti) = INT.borrow(cs).borrow_mut().deref_mut() {
+            // Clear the interrupt flag.
+            exti.pr.modify(|_, w| w.pr5().set_bit());
+
+            // Change the LED state on each interrupt.
+            if let Some(ref mut led) = LED.borrow(cs).borrow_mut().deref_mut() {
+                led.toggle().unwrap();
+            }
+
+        }
+    });
+}
+
+
+#[interrupt]
+fn EXTI15_10() {
+ cortex_m::interrupt::free(|cs| {
+        if let Some(ref mut exti) = INT.borrow(cs).borrow_mut().deref_mut() {
+            // Clear the interrupt flag.
+            exti.pr.write(|w| w.pr10().set_bit());
+
+            // Change the LED state on each interrupt.
+            if let Some(ref mut led) = LED.borrow(cs).borrow_mut().deref_mut() {
+                led.toggle().unwrap();
+            }
+
+        }
+    });
+}
+
 
 #[cfg(feature = "use_semihosting")]
 fn semihosting_print_example() -> Result<(), core::fmt::Error> {
