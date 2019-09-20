666c6bc96389266e5c972edd18691a85ea9ee7cb
Ingmar Jager <ingmarjager@gmail.com>
2019-09-20 12:37:53 +0200

    Add powoer over BMP setting

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -9,7 +9,11 @@ extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to c
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use cortex_m_semihosting::{hprintln};
+
+#[macro_use]
+extern crate cortex_m_semihosting;
+use cortex_m_semihosting::{hprintln, hio};
+use core::fmt::Write;
 
 extern crate stm32f1xx_hal;
 
@@ -17,8 +21,35 @@ extern crate stm32f1xx_hal;
 fn main() -> ! {
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
     hprintln!("Hello, rust world!").unwrap();
+    print();
+
+    const UUID: *mut u32 = 0x0009_FC70 as *mut u32;
+    dbg!(UUID);
+    let mut uuid: [u32; 4] = [0; 4];
+    for i in 0..4 {
+        dbg!(i);
+        uuid[i] = unsafe { dbg!(UUID.offset(i as isize).read_volatile()) };
+    }
 
     loop {
         // your code goes here
     }
+}
+
+// This function will be called by the application
+
+
+// This function will be called by the application
+fn print() -> Result<(), core::fmt::Error> {
+    let mut stdout = match hio::hstdout() {
+        Ok(fd) => fd,
+        Err(()) => return Err(core::fmt::Error),
+    };
+
+    let language = "Rust";
+    let ranking = 1;
+
+    write!(stdout, "{} on embedded is #{}!\n", language, ranking)?;
+
+    Ok(())
 }
