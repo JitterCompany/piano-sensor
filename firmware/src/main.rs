5182b1ce012a5968fe6f86de0e15cb4543911f35
Ingmar Jager <ingmarjager@gmail.com>
2019-09-21 17:54:27 +0200

    Cleanup the code now that it is working

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -28,7 +28,7 @@ use stm32f1xx_hal::{
 
 extern crate embedded_hal;
 use embedded_hal::digital::v2::{InputPin, OutputPin};
-use embedded_hal::{serial, fmt};
+use embedded_hal::{serial};
 
 use core::{cell::RefCell, ops::DerefMut, cell::UnsafeCell};
 use cortex_m::{interrupt::Mutex, interrupt::CriticalSection};
@@ -76,32 +76,6 @@ unsafe impl Sync for CSCounter {}
 static COUNTER: CSCounter = CS_COUNTER_INIT;
 
 
-// fn init_clock() {
-//     use stm32::clock::*;
-//     use stm32::flash;
-
-//     let rcc = stm32::RCC();
-
-//     rcc.enable_hse();
-//     while !rcc.get_hse_ready() {}
-
-//     // use 8 MHz external oscillator
-//     // set PLL to 72 MHz (8 MHz * 9)
-//     rcc.set_pll_multiplier(PLLMultiplier::Mult9);
-//     rcc.set_pll_source(PLLSource::HighSpeedExternal);
-
-//     // set prescaler
-//     rcc.set_apb_1_prescaler(APBPrescaler::Div2);
-
-//     let flash = stm32::FLASH();
-//     flash.enable_prefetch();
-//     flash.set_latency(flash::FlashLatency::TwoWait);
-//     rcc.enable_pll();
-//     while !rcc.get_pll_ready() {}
-
-//     rcc.set_system_clock(SystemClockSwitch::PLLOutput);
-// }
-
 #[entry]
 fn main() -> ! {
     // #[cfg(feature = "use_semihosting")]
@@ -116,14 +90,13 @@ fn main() -> ! {
 
     let mut rcc = dp.RCC.constrain();
 
-//.sysclk(72.mhz()).pclk1(32.mhz())
-    // let clocks: Clocks = rcc.cfgr.use_hse(8.mhz()).hclk(72.mhz()).sysclk(72.mhz()).freeze(&mut flash.acr);
-    let clocks: Clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(72.mhz()).pclk1(36.mhz()).freeze(&mut flash.acr);
+    // configure clocks
+    // let clocks: Clocks = rcc.cfgr.use_hse(8.mhz()).sysclk(72.mhz()).pclk1(36.mhz()).freeze(&mut flash.acr);
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
     #[cfg(feature = "use_semihosting")] {
         hprintln!("sysclk freq: {}", clocks.sysclk().0).unwrap();
     }
 
-    // let clocks = rcc.cfgr.freeze(&mut flash.acr);
 
 
     // configure clock to 72MHz
@@ -347,51 +320,3 @@ fn write_string(tx: &mut impl serial::Write<u8>, s: &str) {
         block!(tx.write(a as u8)).ok();
     }
 }
-
-
-
-
-
-
-// fn enable_uart() {
-//     use stm32::gpio::*;
-//     use stm32::clock::Peripheral;
-
-//     let rcc = stm32::RCC();
-//     rcc.enable_peripheral(Peripheral::USART);
-
-//     // setup USART
-//     let usart1 = stm32::USART_1();
-//     usart1.set_baud(468, 12);
-//     //usart1.set_baud_calc(72000000, 9600);
-//     usart1.enable_transmitter();
-//     usart1.enable_receiver();
-
-//     usart1.enable();
-
-//     rcc.enable_peripheral(Peripheral::AlternateFunctionIO);
-//     rcc.enable_peripheral(Peripheral::IOPortA);
-
-//     let porta = stm32::PORT_A();
-//     // setup output potr
-//     porta.set_port_pin_mode(Pin::P9, PortPinMode::OutputMode50MHz);
-//     porta.set_port_pin_config(Pin::P9, PortPinConfig::AlternateFunctionOutputPushPull);
-//     // setup input port
-//     porta.set_port_pin_config(Pin::P10, PortPinConfig::FloatingInput);
-// }
-
-// fn uart_tx(ch : u8) {
-//     while !stm32::USART_1().get_transmit_empty() {};
-//     stm32::USART_1().send_data(ch);
-// }
-
-// fn print(s : &str) {
-//     for a in s.chars() {
-//         uart_tx(a as u8);
-//     }
-// }
-
-// fn println(s : &str) {
-//     print(s);
-//     uart_tx('\n' as u8);
-// }
